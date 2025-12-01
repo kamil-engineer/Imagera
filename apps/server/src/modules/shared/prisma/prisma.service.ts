302af2b5a2bdcddd6e5b7b11a17c8747e47ecf37
Kamil <mathey.academy@gmail.com>
2025-12-01 19:09:42 +0100

    feat: prisma & eslint

---
+++ apps/server/src/modules/shared/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from './../../../generated/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
+    super({ adapter });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
